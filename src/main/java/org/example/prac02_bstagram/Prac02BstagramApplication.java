95b8e6e3445af1a8910233385169415a1b84b30f
ddarahakit2026 <ddarahakit2026@gmail.com>
2026-02-26 17:33:10 +0900

    [Init] 프로젝트 초기 설정

---
+++ src/main/java/org/example/prac02_bstagram/Prac02BstagramApplication.java
@@ -0,0 +1,13 @@
+package org.example.prac02_bstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prac02BstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prac02BstagramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/prac02_bstagram/Prac02BstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.prac02_bstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prac02BstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
